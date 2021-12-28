85e3cda856cfa962c26cac558890f7e6e7b160af
marcusfonte18 <marcusfonte18@gmail.com>
2021-12-27 23:34:09 -0300

    Firt commit

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/", (request, response) => {
+  return response.json({ message: "Hello Marcus" })
+})
+
+app.listen(3333, () => console.log("Server is running!!"))
+
