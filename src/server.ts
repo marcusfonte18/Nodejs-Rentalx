b8810743e9ff7e3ebf81214e39a7d4271f3f97f0
marcusfonte18 <marcusfonte18@gmail.com>
2021-12-28 19:14:43 -0300

    Criando categorias

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,11 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes'
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello Marcus" })
-})
+app.use(categoriesRoutes);
 
-app.listen(3333, () => console.log("Server is running!!"))
+app.listen(3333, () => console.log("Server is running!!"));
 
