446f6fe54cccd0f72c6fc70a4342eeaa8d7e0bd5
marcusfonte18 <marcusfonte18@gmail.com>
2022-01-25 15:33:19 -0300

    Conhecendo o conceito de stream

--- src/modules/cars/useCases/ImportCategory/ImportCategoryController.ts
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryController.ts
@@ -1,11 +1,12 @@
 import { Request, Response } from 'express'
+
 import { ImportCategoryUseCase } from './ImportCategoryUseCase';
 
 class ImportCategoryController {
   constructor(private importCategoryUseCase: ImportCategoryUseCase) { }
   handle(request: Request, response: Response): Response {
     const { file } = request;
-    this.importCategoryUseCase.execute(file)
+    this.importCategoryUseCase.execute(file);
 
 
     return response.send();
--- src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/ImportCategory/ImportCategoryUseCase.ts
@@ -1,9 +1,19 @@
-
+import fs from "fs" //file-system é nativa do nodejs
+import { parse } from 'csv-parse';
 
 class ImportCategoryUseCase {
 
-  execute(file: any) {
-    console.log(file)
+  execute(file: Express.Multer.File): void {
+
+    const stream = fs.createReadStream(file.path);
+
+    const parseFile = parse();
+
+    stream.pipe(parseFile)
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    })
   }
 }
 
