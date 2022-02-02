995e83e3dd41dbdc414a7f5a615c29c5ae8afc91
marcusfonte18 <marcusfonte18@gmail.com>
2022-02-02 12:41:05 -0300

    Criando token do usuário

---
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.ts
@@ -0,0 +1,61 @@
+import { inject, injectable } from 'tsyringe';
+import { compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+
+import { IUsersRepository } from '../../repositories/IUsersRepository';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    //Usuário Existe
+    const user = await this.usersRepository.findByEmail(email)
+
+    if (!user) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    const passwordMatch = await compare(password, user.password)
+
+    //Senha está correta
+    if (!passwordMatch) {
+      throw new Error('Email or password incorrect!');
+    }
+
+    //Gerar Jsonwebtoken
+    const token = sign({}, "d2537851436276b02eeb9ebf12bb37d1", {
+      subject: user.id,
+      expiresIn: "1d"
+    });
+
+    const tokenReturn: IResponse = {
+      token,
+      user: {
+        name: user.name,
+        email: user.email
+      }
+    }
+
+    return tokenReturn;
+
+  }
+}
+
+export { AuthenticateUserUseCase }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { authenticateRoutes } from './authenticate.routes';
 import { categoriesRoutes } from './categories.routes';
 import { specificationsRoutes } from './specifications.routes';
 import { usersRoutes } from './users.routes';
@@ -9,6 +10,9 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
+
+//É passado a rota direto por que só vai ter o /
+router.use(authenticateRoutes)
 
 
 export { router };
